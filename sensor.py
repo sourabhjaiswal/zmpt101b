d6e5b50a5d907c518fdb06692d60cad87484453b
Sourabh Jaiswal <sourabhjaiswal92@gmail.com>
2020-09-26 23:08:23 +0530

    updated UNIT_VOLTAGE to UNIT_VOLT

--- sensor.py
+++ sensor.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_SENSOR, CONF_ID, ICON_FLASH, UNIT_VOLTAGE
+from esphome.const import CONF_SENSOR, CONF_ID, ICON_FLASH, UNIT_VOLT
 
 CODEOWNERS = ['@sourabhjaiswal']
 
 zmpt101b_ns = cg.esphome_ns.namespace('zmpt101b')
 ZMPT101BSensor = zmpt101b_ns.class_('ZMPT101BSensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLTAGE, ICON_FLASH, 2).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 2).extend({
     cv.GenerateID(): cv.declare_id(ZMPT101BSensor),
 }).extend(cv.polling_component_schema('60s'))
 
