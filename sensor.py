fd376758c60f92200154e3090c73229020da5211
Sourabh Jaiswal <sourabhjaiswal92@gmail.com>
2020-09-27 00:32:40 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -10,7 +10,7 @@ ZMPT101BSensor = zmpt101b_ns.class_('ZMPT101BSensor', sensor.Sensor, cg.PollingC
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 2).extend({
     cv.GenerateID(): cv.declare_id(ZMPT101BSensor),
-    cv.add_library('EmonLib', '1.1.0'),
+    cv.add_library('EmonLib', '1.1.0')
 }).extend(cv.polling_component_schema('60s'))
 
 
