7dae5b8add38a808b971e76a0d0d6a677b23209b
root <root@a0d7b954-esphome.local.hass.io>
2020-09-27 16:45:50 +0530

    Added support for Averaging the voltage

--- zmpt101b.cpp
+++ zmpt101b.cpp
@@ -15,17 +15,39 @@ void ZMPT101BSensor::setup() {
 
 void ZMPT101BSensor::dump_config() {
   LOG_SENSOR("", "ZMPT101B Sensor", this);
-  ESP_LOGCONFIG(TAG, "  Phase Shift: %f, TimeOut: %d, Calibration: %f", this->phase_shift_,this->timeout_,this->calibration_);
+  ESP_LOGCONFIG(TAG, "  Phase Shift: %f, Sample Duration: %d, Calibration: %f", this->phase_shift_,this->number_of_samples_*this->frequency_,this->calibration_);
   LOG_UPDATE_INTERVAL(this);
 }
 
 void ZMPT101BSensor::update() {
-  float supplyVoltage   = this->emon1.Vrms;             //extract Vrms into Variable
-  this->publish_state(supplyVoltage);
+  // Set timeout for ending sampling phase
+    this->set_timeout("read", this->number_of_samples_*this->frequency_, [this]() {
+        this->is_sampling_ = false;
+    
+        if (this->num_samples_ == 0) {
+          // Shouldn't happen, but let's not crash if it does.
+          this->publish_state(NAN);
+          return;
+        }
+    
+        float sampled_voltage = this->sample_sum_ / this->num_samples_;
+        //ESP_LOGD(TAG, "'%s' - Raw Value: %.2fA", this->name_.c_str(), irms);
+        this->publish_state(sampled_voltage);
+      });
+
+  // Set sampling values
+  this->is_sampling_ = true;
+  this->num_samples_ = 0;
+  this->sample_sum_ = 0.0f;
 }
 
 void ZMPT101BSensor::loop() {
-  this->emon1.calcVI(20,this->timeout_);
+    this->emon1.calcVI(20,this->frequency_);
+    float supplyVoltage   = this->emon1.Vrms;             //extract Vrms into Variable
+    if (isnan(supplyVoltage))
+        return;
+    this->sample_sum_ += supplyVoltage;
+    this->num_samples_++;
 }
 
 }  // namespace zmpt101b
--- zmpt101b.h
+++ zmpt101b.h
@@ -20,16 +20,21 @@ class ZMPT101BSensor : public sensor::Sensor, public PollingComponent {
   }
   void set_pin(uint8_t pin) { this->pin_ = pin; }
   void set_conf_calibration(float calibration) { this->calibration_ = calibration; }
-  void set_conf_timeout(uint32_t timeout) { this->timeout_ = timeout; }
+  void set_conf_number_of_samples(uint32_t number_of_samples) { number_of_samples_ = number_of_samples; }
+  void set_conf_frequency(uint32_t frequency) { this->frequency_ = frequency; }
   void set_conf_phase_shift(float phase_shift) { this->phase_shift_ = phase_shift; }
 
  protected:
 
   float phase_shift_;
   float calibration_;
-  uint32_t timeout_;
+  uint32_t number_of_samples_;
+  uint32_t frequency_;
   EnergyMonitor emon1;             // Create an instance
   uint8_t pin_;
+  float sample_sum_ = 0.0f;
+  uint32_t num_samples_ = 0;
+  bool is_sampling_ = false;
 };
 
 }  // namespace zmpt101b
